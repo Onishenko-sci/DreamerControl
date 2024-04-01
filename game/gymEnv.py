3f7bd41bc6f4b922435842ccae9875ce967e854b
Onishenko-sci <tolyaonishenko@gmail.com>
2024-04-02 02:14:28 +0500

    Add train.py

--- game/gymEnv.py
+++ game/gymEnv.py
@@ -3,85 +3,108 @@ import pygame
 import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
+import cv2
+
 from classes import *
 
+
 key_to_action = {
-    0: pygame.K_SPACE,
-    1: pygame.K_UP,
-    2: pygame.K_RIGHT,
-    3: pygame.K_LEFT
+    0: pygame.K_UP,
+    1: pygame.K_RIGHT,
+    2: pygame.K_LEFT,
+    4: pygame.K_SPACE
 }
 
 class room_env(gym.Env):
-    def __init__(self, goal, obstacles_n):
-        self.game = Game(level=goal, obstacles_n=obstacles_n)
-        self.action_space = spaces.Discrete(4)
-        self.observation_space = spaces.Box(
-            low=0, high=255, shape=(640, 480, 3), dtype=np.uint8)
+    def __init__(self):
+        self.game = Game('laser', 0)
+        self.action_space = spaces.Discrete(3)
+        self.observation_space = spaces.Box(low=0, high=255, shape=( 100, 100, 1 ), dtype=np.uint8)
+        #self.observation_space = spaces.Box(low=0, high=361, shape=(5, ), dtype=np.int16)
+
         self.terminated = False
         self.truncated = False
         self.render_mode = None
         self.info = {}
+        self.temp = 0
+        self.valid_timestep = 100
+        self.dist = 0
+        self.prev_dist = 0
+
 
     def step(self, action):
         # Make step
         self.game.do_event(key_to_action[action])
-        space.step(0.1)
+        space.step(1)
         # Create observation
         self.game.draw()
-        self.observation = np.transpose(
-            np.array(pygame.surfarray.pixels3d(screen)), axes=(0, 1, 2))
-        
+
+        cadr = np.transpose(np.array(pygame.surfarray.pixels3d(screen)), axes=(0, 1, 2))
+
+        self.observation = (cadr[:,:,0])[:,:,np.newaxis]
 
         # Punishment for wasting time
         self.timestep_passed += 1
         self.reward = -self.timestep_passed // self.valid_timestep
 
-        if self.timestep_passed > self.valid_timestep*100:
-            self.reward = -100
-            self.terminated = True
-
         # Lose condition
         if self.game.lose:
-            self.reward = -100
+            self.reward = -200
             self.terminated = True
 
         # Win condition
-        if self.game.win_condition():
-            self.reward = 1000
+        if self.game.robot_touch_laser():
+            self.reward = 200
             self.terminated = True
 
         if self.render_mode == 'human':
+            print('Action ', action)
+            print('Reward: ', self.reward)
             self.render()
+
+        #Change observation to Robot pos, Robot angle, Lazer pos
+        #self.observation = self.short_obs()
 
         return self.observation, self.reward, self.terminated, self.truncated, self.info
 
+    def short_obs(self):
+      robot_pos = self.game.robot.body.position
+      robot_angle = int(360*self.game.robot.body.angle/(2*math.pi))
+      if robot_angle > 360: robot_angle= robot_angle-360
+      if robot_angle < 0: robot_angle= robot_angle+360
+      laser_pos = self.game.laser.center
+      short_observation = np.array([robot_pos[0],robot_pos[1],robot_angle,laser_pos[0],laser_pos[1]], dtype=int)
+      return short_observation
+
     def reset(self, seed=None, options=None):
+        self.temp = self.temp + 1
+        #print(self.temp)
+
         super().reset(seed=seed)
         self.game.reset_game()
         self.game.draw()
-        self.observation = np.transpose(
-            np.array(pygame.surfarray.pixels3d(screen)), axes=(0, 1, 2))
+
+        cadr = np.transpose(np.array(pygame.surfarray.pixels3d(screen)), axes=(0, 1, 2))
+        self.observation = (cadr[:,:,0])[:,:,np.newaxis]
 
         # reset reward
         self.reward = 0
         self.score = 0
-        self.valid_timestep = 100
         self.timestep_passed = 0
         self.terminated = False
         self.truncated = False
 
         if self.render_mode == 'human':
+            
             self.render()
 
-        self.info = [self.game.robot.body.position, self.game.robot.body.angle, self.game.laser.center]
-
+        #self.observation = self.short_obs()
         return self.observation, self.info
 
-    def render(self, render_mode='human'):
-        self.render_mode = render_mode
+    def render(self):
         pygame.display.flip()
-        clock.tick(4)
+        clock.tick(10)
+
 
     def close(self):
         pygame.quit()
--- game/train.py
+++ game/train.py
@@ -1,27 +1,24 @@
 import torch
 import time
+# pip install stable-baselines3[extra]
 from stable_baselines3 import PPO
-import stable_baselines3
+from gymEnv import *
 
-start_time = time.time()
-iterations = 100000
-
-Log_path = os.path.join('Training', 'logs')
-
+# Initialization
 env2 = room_env()
 model = PPO('CnnPolicy', env2, verbose=1)
-
+iterations = 2048
+# Learning (with timing)
 env2.render_mode = None
+start_time = time.time()
 model.learn(total_timesteps=iterations)
-
 end_time = time.time()
 elapsed_time = end_time - start_time
-print(f"Time for {iterations} iterations: {elapsed_time} seconds")
-
-
+print(f"Time for {iterations} iterations: {elapsed_time/3600} hours")
+# Visual evaluation
 env2.render_mode = 'human'
-model.learn(3)
-
-model.save('EasyModel')
-torch.save(model.policy.state_dict(),'5inPolisymodel.pt')
+model.learn(2048)
+# Saving
+model.save('CnnModel')
+torch.save(model.policy.state_dict(),'policy.pt')
 
